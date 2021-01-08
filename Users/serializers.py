ffa671f48e23614ed6c5681b4dd76fb27a126434
CMierez <carlos.mierez20@gmail.com>
2021-01-08 16:16:42 -0300

    Cambios al User Serializer para que acepte password

--- Users/serializers.py
+++ Users/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+    
+    role = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    
     class Meta:
         model = User
-        fields = ['url', 'username', 'is_staff',]
+        fields = ['url', 'username', 'is_staff', 'password', 'role',]
+    
+    
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
     
     # def create(self, validated_data):
     #     groups_data = validated_data.pop('groups')
